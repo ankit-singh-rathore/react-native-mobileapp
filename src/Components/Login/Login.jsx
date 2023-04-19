b10b42c9d692ec73c5da370541c7d9b4f7bd3b1c
Ankit Singh Rathore <ankit.singh.rathore@accenture.com>
2023-04-19 12:16:02 +0530

    Authenticte app with default mobile app authenticator

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,26 @@
-import React, { useState} from "react";
-import { Button, StyleSheet, TextInput, View, SafeAreaView  } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+  SafeAreaView,
+  Text
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as LocalAuthentication from "expo-local-authentication";
+import { 
+  hasHardwareAsync,
+  isEnrolledAsync,
+  authenticateAsync 
+} from 'expo-local-authentication';
 
 function Login(props) {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   function handleEmail(value) {
     setEmail(value);
   }
@@ -20,17 +34,31 @@ function Login(props) {
       : alert("Please provide correct details.");
   }
 
+  const biometricsAuth = async () => {
+    const compatible = await hasHardwareAsync()
+    if (!compatible) throw 'This device is not compatible for biometric authentication'
+    const enrolled = await isEnrolledAsync()
+    if (!enrolled) throw 'This device dont have biometric authentication enabled'
+    const result = await authenticateAsync()
+    if (!result.success) throw `${result.error} - Authentication unsuccessful`
+    return
+}
+
   const storeDataAndNavigate = async (value) => {
     try {
-      await AsyncStorage.setItem('userEmail', email)
-      navigation.navigate("CakesList")
+      await AsyncStorage.setItem("userEmail", email);
+      navigation.navigate("CakesList");
     } catch (e) {
       // saving error
     }
-  }
+  };
+
+  useEffect(() => {
+    biometricsAuth();
+  }, []);
 
   return (
-    <SafeAreaView >
+    <SafeAreaView>
       <View>
         <TextInput
           style={styles.input}
